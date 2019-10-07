b3f952b93ad9bf23e39842f846477cc9fab1b2ba
Fernando Diaz M <fer.diaz@udla.edu.co>
2019-10-07 09:09:01 -0500

    Mejora Frontend Login - Principal

--- Login/js/app.js
+++ Login/js/app.js
@@ -1,19 +1,55 @@
-var user = document.getElementById('user');
-var pass = document.getElementById('pass');
+var user = document.getElementById("user");
+var pass = document.getElementById("pass");
 
+// function inicio() {
+//   if (user.value == "admin" && pass.value == "admin") {
+//     console.log("Credenciales validas");
+//     window.location = "Principal.html";
+//   } else {
+//     console.log("Credenciales incorrectas");
+//   }
+// }
 
+var iniciosesion;
 
+function inicio() {
+  if (user.value == "admin" && pass.value == "admin") {
+    iniciosesion = window.open("Principal.html");
+  }
+}
 
-function inicio(){
+function Cerrar() {
+  window.close();
+}
 
-    if (user.value == "admin" && pass.value == "admin") {
-        console.log("Credenciales validas");
-        window.location = '../../index.html';
-        
-    }
-    else{
-        console.log("Credenciales incorrectas");
-        
-    }   
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDLsFQ5raJxVMwScErCpQrThc1G6c-bT-U",
+  authDomain: "v2famacebu.firebaseapp.com",
+  databaseURL: "https://v2famacebu.firebaseio.com",
+  projectId: "v2famacebu",
+  storageBucket: "v2famacebu.appspot.com",
+  messagingSenderId: "1024538402660",
+  appId: "1:1024538402660:web:30c63948e4ffe60c7ca471"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+var db = firebase.firestore();
+
+// Agregando datos
+
+function RegistrarPersona() {
+  db.collection("users")
+    .add({
+      nombre: "Ada",
+      apellido: "Lovelace",
+      edad: 18
+    })
+    .then(function(docRef) {
+      console.log("Documento creado con el ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error: ", error);
+    });
 }
